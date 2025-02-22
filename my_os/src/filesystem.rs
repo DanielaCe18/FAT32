93faf5d02f3e8eb824a86cb8dd297a06d36ff991
DanielaCe18 <cedaniela613@gmail.com>
2025-02-22 10:07:50 -0500

    read write and allocate a cluster

--- my_os/src/filesystem.rs
+++ my_os/src/filesystem.rs
@@ -1,3 +1,5 @@
+//! Simple FAT32 Filesystem Implementation
+
 use crate::directory::cluster::Cluster;
 use crate::directory::table::FatValue;
 use spin::Mutex;
@@ -17,3 +19,39 @@ impl<S: StorageDevice> FatFileSystem<S> {
             cluster_size,
         }
     }
+
+    /// Read a cluster from the filesystem.
+    pub fn read_cluster(&self, cluster: Cluster) -> Option<Vec<u8>> {
+        let mut buffer = vec![0; self.cluster_size as usize];
+        let offset = self.partition_start + cluster.to_offset(self.cluster_size);
+
+        if self.storage_device.lock().read(offset, &mut buffer).is_ok() {
+            Some(buffer)
+        } else {
+            None
+        }
+    }
+
+    /// Write data to a cluster.
+    pub fn write_cluster(&self, cluster: Cluster, data: &[u8]) -> bool {
+        let offset = self.partition_start + cluster.to_offset(self.cluster_size);
+        self.storage_device.lock().write(offset, data).is_ok()
+    }
+
+    /// Allocate a new cluster.
+    pub fn allocate_cluster(&self) -> Option<Cluster> {
+        for cluster_id in 2..0xFFFF_FFF8 { // Skip reserved clusters
+            if let FatValue::Free = FatValue::get(self, Cluster(cluster_id)) {
+                FatValue::put(self, Cluster(cluster_id), FatValue::EndOfChain);
+                return Some(Cluster(cluster_id));
+            }
+        }
+        None
+    }
+
+    /// Free a cluster.
+    pub fn free_cluster(&self, cluster: Cluster) {
+        FatValue::put(self, cluster, FatValue::Free);
+    }
+}
+
