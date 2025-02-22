3e469fc591fdc9e5118d4ec2218ac8a795f8857f
DanielaCe18 <cedaniela613@gmail.com>
2025-02-22 08:03:10 -0500

    module imports in lib

--- my_os/src/lib.rs
+++ my_os/src/lib.rs
@@ -1,3 +1,15 @@
+pub mod attribute;
+pub mod datetime;
+pub mod name;
+pub mod offset_iter;
+pub mod directory;
+pub mod filesystem;
+pub mod memory;
+pub mod process;
+pub mod scheduler;
+pub mod syscall;
+
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
