79c1d0f74c9fa55ad4c282ba162ea9a6dd85c05a
DanielaCe18 <cedaniela613@gmail.com>
2025-02-23 10:04:05 -0500

    implementing process code as running and completed

--- my_os/src/process/process.rs
+++ my_os/src/process/process.rs
@@ -16,3 +16,29 @@ pub struct Process {
     pub state: ProcessState,
     pub name: &'static str,
 }
+
+impl Process {
+    pub fn new(name: &'static str) -> Self {
+        static NEXT_PID: AtomicU64 = AtomicU64::new(1);
+        let pid = NEXT_PID.fetch_add(1, Ordering::Relaxed);
+
+        Process {
+            pid,
+            state: ProcessState::Ready,
+            name,
+        }
+    }
+
+    /// Mark the process as running.
+    pub fn run(&mut self) {
+        self.state = ProcessState::Running;
+        println!("Process {} (PID: {}) is running", self.name, self.pid);
+    }
+
+    /// Mark the process as completed.
+    pub fn terminate(&mut self) {
+        self.state = ProcessState::Terminated;
+        println!("Process {} (PID: {}) terminated", self.name, self.pid);
+    }
+}
+
