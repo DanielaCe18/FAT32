cff5d9555cb01b3801a8c4d4c469b71d36aa1552
DanielaCe18 <cedaniela613@gmail.com>
2025-02-23 09:55:30 -0500

    adding robin round for new processes

--- my_os/src/scheduler.rs
+++ my_os/src/scheduler.rs
@@ -29,3 +29,24 @@ impl Scheduler {
             next_pid: AtomicU64::new(1),
         }
     }
+
+    /// Add a new process to the scheduler.
+    pub fn add_process(&mut self, process: Process) {
+        let scheduled = ScheduledProcess { process, waker: None };
+        self.queue.push_back(scheduled);
+    }
+
+    /// Get the next process to run (Round-Robin).
+    pub fn next_process(&mut self) -> Option<ScheduledProcess> {
+        while let Some(mut scheduled) = self.queue.pop_front() {
+            // Only return if the process is ready to run.
+            if scheduled.process.state == ProcessState::Ready {
+                scheduled.process.state = ProcessState::Running;
+                return Some(scheduled);
+            } else {
+                // If not ready, push back and check the next one.
+                self.queue.push_back(scheduled);
+            }
+        }
+        None
+    }
